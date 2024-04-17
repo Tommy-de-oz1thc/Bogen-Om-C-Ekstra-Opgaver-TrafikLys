7c245c1757b035ca6a5f2f56ff180e8203ec895b
Tommy-de-oz1thc <60524067+Tommy-de-oz1thc@users.noreply.github.com>
2024-04-17 19:53:55 +0200

    Add files via upload

--- TrafikLys/Program.cs
+++ TrafikLys/Program.cs
@@ -1,1 +1,94 @@
+﻿using System;
 
+namespace cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a TrafficLight object
+            TrafficLight trafficLight = new TrafficLight();
+            int max_run = 10;
+            Console.WriteLine($"The loop will run for {max_run} times");
+            // Run state changes 10 times when creating the object
+            for (int ac = 0; ac < max_run; ac++)
+            {
+                trafficLight.ChangeState(); // Call the ChangeState method on the created object
+
+                // Clear the console screen
+                Console.Clear();
+
+                // Print the state using the GetState method
+                //Console.WriteLine(trafficLight.GetState());
+
+                // Display the traffic light image based on the state
+                switch (trafficLight.GetState())
+                {
+                    case "Red":
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(@"
+                         ____
+                        /    \
+                       / Stop \
+                      /________\
+                      |        |
+                      |  Red   |
+                      |        |
+                      \________/
+");
+                        break;
+                    case "RedYellow":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(@"
+                         ____
+                        /    \
+                       / Stop \
+                      /________\
+                      |        |
+                      |  Red   |
+                      | Yellow |
+                      \________/
+                          
+");
+                        break;
+                    case "Green":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(@"
+                         ____
+                        /    \
+                       /  Go  \
+                      /________\
+                      |        |
+                      |  Green |
+                      |        |
+                      \________/
+");
+                        break;
+                    case "Yellow":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(@"
+                         ____
+                        /    \
+                       / Stop \
+                      /________\
+                      |        |
+                      | Yellow |
+                      |        |
+                      \________/
+");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid state!");
+                        break;
+                }
+
+                // Wait for a moment before the next iteration
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TrafikLys/TrafficLight.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace cs
+{
+    public class TrafficLight
+    {
+        // Opret en liste over tilstande for trafiklyset
+        List<string> trafficStates = new List<string> { "Red", "RedYellow", "Green", "Yellow" };
+        private string state = "";
+        private int trafficnum = 0;
+
+        // Default constructor
+        public TrafficLight()
+        {
+            // Her kan du evt. initialisere trafiklyset til en bestemt starttilstand
+            state = trafficStates[trafficnum];
+
+
+        }
+
+        public void ChangeState()
+        {
+            // Kontroller, om trafiknum er større end længden af trafiklysets tilstande minus 1
+            if (trafficnum >= trafficStates.Count - 1)
+            {
+                // Hvis trafiknum er uden for gyldige indeks, så nulstil det
+                trafficnum = 0;
+            }
+            else
+            {
+                // Ellers, øg trafiknum for at skifte til næste tilstand
+                trafficnum++;
+            }
+
+            // Skift tilstanden baseret på trafiknum
+            switch (trafficnum)
+            {
+                case 0:
+                    // Skift tilstand fra Yellow til Red
+                    Thread.Sleep(2000); // Venter i 2 sekunder
+                    break;
+                case 1:
+                    // Skift tilstand fra Red til RedYellow
+                    Thread.Sleep(5000); // Venter i 5 sekunder
+                    break;
+                case 2:
+                    // Skift tilstand fra RedYellow til Green
+                    Thread.Sleep(2000); // Venter i 2 sekunder
+                    break;
+                case 3:
+                    // Skift tilstand fra Green til Yellow
+                    Thread.Sleep(5000); // Venter i 5 sekunder
+                    break;
+            }
+
+            // Opdater tilstand
+            state = trafficStates[trafficnum];
+        }
+
+        public string GetState()
+        {
+            return state;
+        }
+    }
+}
