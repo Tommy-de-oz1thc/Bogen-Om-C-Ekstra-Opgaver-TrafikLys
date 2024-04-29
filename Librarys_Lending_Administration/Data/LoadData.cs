023fd503c8ffef1404a2b280df52149284da767a
Tommy-de-oz1thc <60524067+Tommy-de-oz1thc@users.noreply.github.com>
2024-04-29 06:53:51 +0200

    Add files via upload

--- Librarys_Lending_Administration/Data/LoadData.cs
+++ Librarys_Lending_Administration/Data/LoadData.cs
@@ -1,1 +1,26 @@
+﻿using Library_s_Lending_Administration.Lending;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Library_s_Lending_Administration.Data.Load;
 
+namespace Library_s_Lending_Administration.Data
+{
+    public class LoadData
+    {
+
+        
+        public LoadData() 
+        {
+            LoadPerson loadDataPerson = new LoadPerson();
+          //LoadAdultBooks loadDataAdultBooks = new LoadAdultBooks();   
+          //LoadChildrenBooks loadDataChildrenBooks = new LoadChildrenBooks();
+          //LoadCartoons loadDataCartoons = new LoadCartoons(); 
+          //LoadCDs loadDataCDs = new LoadCDs();
+            LoadForum loadDataForum = new LoadForum();
+            LoadLendings loadDataLending = new LoadLendings();
+          //LoadDVDMovies loadDataDVDMovies = new LoadDVDMovies();
+          //LoadRecords loaddataRecords = new LoadRecords();              
+        }
+    }
+}
---
+++ Librarys_Lending_Administration/Data/SetUpData.cs
@@ -0,0 +1,47 @@
+﻿using Library_s_Lending_Administration.Data.SetUp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_s_Lending_Administration.Data
+{
+    public class SetUpData
+    {
+        public SetUpData()
+        {
+
+                try
+                {
+
+                    // Specifiser stien til mappen
+                    string dataDirectory = @"Data";
+
+                    // Sjekk om mappen eksisterer
+                    if (!Directory.Exists(dataDirectory))
+                    {
+                        // Opprett mappen hvis den ikke eksisterer
+                        Directory.CreateDirectory(dataDirectory);
+                    }
+
+                    SetUpPerson SetUpDataPerson = new SetUpPerson();
+                    SetUpAdultBooks SetupDataAdultBooks = new SetUpAdultBooks();
+                    SetUpChildrenBooks SetUpDataChildrenBooks = new SetUpChildrenBooks();
+                    SetUpCartoons SetUpDataCartoons = new SetUpCartoons();
+                    SetUpCDs SetUpDataCDs = new SetUpCDs();
+                    SetUpDVDMovies SetUpDataDVDMovies = new SetUpDVDMovies();
+                    SetUpRecords SetUpDataRecords = new SetUpRecords();
+                    SetUpForum SetUpDataForum = new SetUpForum();
+                    SetUpLendings SetUpDataLendings = new SetUpLendings();
+                    SetUpPayments setUpDataPayments = new SetUpPayments();
+
+                }
+                catch (Exception ex) { MessageBox.Show($"Error: {ex.Message}"); }
+        }
+       
+    
+    }
+}
